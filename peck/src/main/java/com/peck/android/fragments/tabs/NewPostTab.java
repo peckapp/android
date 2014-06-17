a43f52c37e602b07a58efc2d919a2eb076150fdb
mammothbane <itsamammoth@gmail.com>
2014-06-17 17:22:21 -0400

    working on the new item tab

--- peck/src/main/java/com/peck/android/fragments/tabs/NewPostTab.java
+++ peck/src/main/java/com/peck/android/fragments/tabs/NewPostTab.java
@@ -2,9 +2,12 @@ package com.peck.android.fragments.tabs;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TabHost;
 
 import com.peck.android.R;
 import com.peck.android.interfaces.Singleton;
@@ -18,7 +21,29 @@ public class NewPostTab extends Fragment implements BaseTab {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.frag_newpost, container, false);
+
+        (Button)getActivity().findViewById(R.id.bt_event).setOnClickListener(newPostListener(new ));
+
     }
+
+    private class newPostListener implements View.OnClickListener {
+        Fragment f;
+
+        public newPostListener(Fragment f) {
+            this.f = f;
+        }
+
+        @Override
+        public void onClick(View view) {
+            FragmentTransaction ft = getActivity().getSupportFragmentManager().beginTransaction();
+
+            if (getActivity().getSupportFragmentManager().findFragmentByTag(f.getTag()))
+
+
+            ft.commit();
+        }
+    }
+
 
 
     @Override
