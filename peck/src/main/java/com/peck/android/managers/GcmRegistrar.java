90091294bcf7366be9368b9425c1227df64ab4a9
mammothbane <nathan@peckapp.com>
2014-08-12 10:40:46 -0400

    commented gcmregistrar

--- peck/src/main/java/com/peck/android/managers/GcmRegistrar.java
+++ peck/src/main/java/com/peck/android/managers/GcmRegistrar.java
@@ -3,6 +3,7 @@ package com.peck.android.managers;
 import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.support.annotation.Nullable;
 import android.util.Log;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -16,6 +17,9 @@ import java.io.IOException;
  * Created by mammothbane on 8/8/2014.
  *
  * a class to register with gcm.
+ *
+ * @since 1.0
+ * @author mammothbane
  */
 public class GcmRegistrar {
     public static final String GCM_SENDER_ID = "651374007309";
@@ -23,7 +27,13 @@ public class GcmRegistrar {
     private final static String PROPERTY_REG_ID = "registration_id";
     private final static int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
 
-    public static boolean checkPlayServices(Activity context) {
+    /**
+     * check to see if google play services are installed. notify the given activity if not.
+     * @param context display an alertdialog here if GPS aren't installed. can be null.
+     * @return true if google play services are available. false if not.
+     * @since 1.0
+     */
+    public static boolean checkPlayServices(@Nullable Activity context) {
         int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(PeckApp.getContext());
         if (resultCode != ConnectionResult.SUCCESS) {
             if (context != null) {
@@ -40,6 +50,12 @@ public class GcmRegistrar {
         return true;
     }
 
+    /**
+     * blocking method to register with gcm servers.
+     *
+     * @return the registration token received from gcm. null on failure.
+     * @since 1.0
+     */
     public static String register() {
         if (checkPlayServices(null)) {
             gcm = GoogleCloudMessaging.getInstance(PeckApp.getContext());
@@ -59,9 +75,17 @@ public class GcmRegistrar {
         }
     }
 
-
+    /**
+     * get the current registration id. makes no network access.
+     * @return whatever's in the local shared preferences. empty string if nothing present.
+     * @since 1.0
+     */
     private static String getRegistrationId() {
         final SharedPreferences prefs = getGCMPreferences();
+        if (prefs.getFloat("version", 0f) < PeckApp.version) { //if we've upgraded, re-register. gcm might not work/work the same on a new version
+            prefs.edit().putString(PROPERTY_REG_ID, "").apply();
+            return "";
+        }
         String registrationId = prefs.getString(PROPERTY_REG_ID, "");
         if (registrationId.isEmpty()) {
             Log.i(GcmRegistrar.class.getSimpleName(), "Registration not found.");
@@ -72,9 +96,11 @@ public class GcmRegistrar {
     }
 
 
+    /**
+     * @return the sharedprefs for gcm
+     * @since 1.0
+     */
     private static SharedPreferences getGCMPreferences() {
-        // This sample app persists the registration ID in shared preferences, but
-        // how you store the regID in your app is up to you.
         return PeckApp.getContext().getSharedPreferences(PeckApp.class.getSimpleName(),
                 Context.MODE_PRIVATE);
     }
