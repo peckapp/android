1767a1f2fc999a6c685bbc6853750282de849730
mammothbane <nathan@peckapp.com>
2014-08-12 10:03:59 -0400

    trying to get annotations to be lowercase

---
+++ peck/src/main/java/com/peck/android/annotations/DBType.java
@@ -0,0 +1,17 @@
+package com.peck.android.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by mammothbane on 7/1/2014.
+ * database type for a given model field
+ */
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface DBType {
+    String value() default "text";
+}
